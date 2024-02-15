a808fe097c341a15f7a516c2ac88d3d0245cac3a
laurel3394 <144968502+laurel3394@users.noreply.github.com>
2024-02-15 15:52:12 +0900

    [feat] 친구기능

--- star_project/Assets/3.Script/JGD/ClassInfo/StageInfo_JGD.cs
+++ star_project/Assets/3.Script/JGD/ClassInfo/StageInfo_JGD.cs
@@ -4,6 +4,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Obstacle_ID
+{
+    none = -1,
+    StartPositopn=0,
+    NormalWall,
+    RedWall,
+    Star,
+    BigStar,
+    Heart,
+    BigHeart,
+    Alphabet
+
+}
+
+
 public class StageInfo_JGD
 {
     public int StageID;
@@ -13,5 +28,28 @@ public class StageInfo_JGD
     {
         Stage_score_Json = Int32.Parse(json["Stage_score_Json"].ToString());
     }
+
+}
+
+public class StageObjectInfo_JGD
+{
+    public List<StageObjectInfo_JGD> StageObject = new List<StageObjectInfo_JGD>();
+    public Obstacle_ID obstacle_ = Obstacle_ID.none; 
+    public int Pos_X;
+    public int Pos_Y;
+
+    public StageObjectInfo_JGD()
+    {
+
+    }
+
+    public StageObjectInfo_JGD(Obstacle_ID obstacle_ID , int positionx, int positiony)
+    {
+        obstacle_ = obstacle_ID;
+        Pos_X = positionx; 
+        Pos_Y = positiony;
+    }
+
+
 
 }
