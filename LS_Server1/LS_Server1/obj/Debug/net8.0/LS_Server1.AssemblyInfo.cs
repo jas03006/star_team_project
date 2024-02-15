45bbca94572040fd6e3831f9598530adfd972128
jas03006 <jas03006@kaist.ac.kr>
2024-02-15 12:59:50 +0900

    ..

--- LS_Server1/LS_Server1/obj/Debug/net8.0/LS_Server1.AssemblyInfo.cs
+++ LS_Server1/LS_Server1/obj/Debug/net8.0/LS_Server1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LS_Server1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8027816873cfce234084ed214e0399293865c8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eddb38852da7c4f6d1d97fff01daf8e28756f8bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LS_Server1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LS_Server1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
