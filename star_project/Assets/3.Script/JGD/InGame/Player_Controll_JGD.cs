fe3e9a43653b2205f5a0af6c679a70fa794b5f39
laurel3394 <144968502+laurel3394@users.noreply.github.com>
2024-02-16 10:58:23 +0900

    [feat] 인게임 생성 시스템

--- star_project/Assets/3.Script/JGD/InGame/Player_Controll_JGD.cs
+++ star_project/Assets/3.Script/JGD/InGame/Player_Controll_JGD.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class Player_Controll_JGD : MonoBehaviour
 {
+    [SerializeField] GameObject camera;
     [SerializeField] private float Jump;
     [SerializeField] public float Speed;
     public Rigidbody2D rigi;
+    
 
     private void Awake()
     {
         rigi = GetComponent<Rigidbody2D>();
+       
     }
-
     private void Update()
     {
-        transform.Translate(Speed * Time.deltaTime, 0, 0);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(this.transform.position.x >=camera.transform.position.x)
         {
-            rigi.velocity = Vector2.up * Jump;
+            camera.transform.position = new Vector3(this.transform.position.x, 3, -3);
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        transform.Translate(Speed * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigi.velocity = Vector2.up *Jump;
         }
     }
+
+
+
 }
--- star_project/Assets/3.Script/JGD/NewGeneration/UI/Stage/StageItemInfo_JGD.cs
+++ star_project/Assets/3.Script/JGD/NewGeneration/UI/Stage/StageItemInfo_JGD.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
 public enum Obstacle_ID
@@ -25,13 +26,12 @@ public class StageItemInfo_JGD : MonoBehaviour
     public int Pos_Y;
     public GameObject Object;
 
-
     public List<GameObject> Objects = new List<GameObject>();
-    string indd = "1:2:3,3:4:1,2:4:2,1:1:1";
+    string indd = "1:20:4,2:4:1,2:10:2,1:15:0";
 
     private void Start()
     {
-
+        ReadStage(indd);
         Debug.Log("-¿Ï-");
     }
 
@@ -49,9 +49,9 @@ public class StageItemInfo_JGD : MonoBehaviour
         {
             list.Add(cutObj[i].Split(':'));
 
-            Debug.Log(list[i][0]);
-            Debug.Log(list[i][1]);
-            Debug.Log(list[i][2]);
+            //Debug.Log(list[i][0]);
+            //Debug.Log(list[i][1]);
+            //Debug.Log(list[i][2]);
         }
 
         for (int i = 0; i < list.Count; i++)
@@ -61,9 +61,9 @@ public class StageItemInfo_JGD : MonoBehaviour
             Pos_Y = int.Parse(list[i][2]);
 
             Object.transform.position = new Vector2(Pos_X,Pos_Y);
+            Debug.Log($"{Objects[ObjectNum].ToString()}, {Pos_X}, {Pos_Y}");
 
-
-            Instantiate(Objects[ObjectNum], Object.transform);
+            Instantiate(Objects[ObjectNum], Object.transform.position,Object.transform.rotation);
         }
 
     }
