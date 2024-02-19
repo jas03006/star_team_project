1956919efb45b94cde5da4f522b5249cb378f538
laurel3394 <144968502+laurel3394@users.noreply.github.com>
2024-02-19 10:12:13 +0900

    [feat] Stage string info

--- star_project/Assets/3.Script/JGD/NewGeneration/UI/Stage/Stage_string_JGD.cs
+++ star_project/Assets/3.Script/JGD/NewGeneration/UI/Stage/Stage_string_JGD.cs
@@ -9,12 +9,38 @@ public class Stage_string_JGD
     public string Stage0_3;
     public string Stage0_4;
     public string Stage0_5;
-    public string Stage1_0;
+
     public string Stage1_1;
     public string Stage1_2;
     public string Stage1_3;
     public string Stage1_4;
     public string Stage1_5;
+
+    public string Stage2_1;
+    public string Stage2_2;
+    public string Stage2_3;    
+    public string Stage2_4;
+    public string Stage2_5;
+
+    public string Stage3_1;
+    public string Stage3_2;
+    public string Stage3_3;
+    public string Stage3_4;
+    public string Stage3_5;
+
+    public string Stage4_1;
+    public string Stage4_2;
+    public string Stage4_3;
+    public string Stage4_4;
+    public string Stage4_5;
+
+    public string Stage5_1;
+    public string Stage5_2;
+    public string Stage5_3;
+    public string Stage5_4;
+    public string Stage5_5;
+
+
         
 
 
