ae0630ad835af7a3bb205517c1caf9e5824a3794
jas03006 <jas03006@kaist.ac.kr>
2024-03-04 10:58:59 +0900

    [fix] 카메라 배율 버그 수정, 비행성 이동시 UI 끄기 추가

--- star_project/Assets/3.Script/TG/Housing/Camera_My_Planet.cs
+++ star_project/Assets/3.Script/TG/Housing/Camera_My_Planet.cs
@@ -27,8 +27,8 @@ public class Camera_My_Planet : MonoBehaviour
 
     public void init()
     {        
-        transform.position = TCP_Client_Manager.instance.my_player.transform.position - transform.forward * distance*3f;
-        center_pos = -transform.forward * distance * 3f;
+        transform.position = TCP_Client_Manager.instance.my_player.transform.position - transform.forward * distance*4f;
+        center_pos = -transform.forward * distance * 4f;
     }
     private Vector2 nowPos, prePos;
     private Vector3 movePos;
