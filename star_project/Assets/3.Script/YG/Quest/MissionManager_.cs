a10b7d3a5729c4ce15b6ac372fc9b5ce5b5d96bf
Hansiban <128816863+Hansiban@users.noreply.github.com>
2024-03-07 15:50:11 +0900

    [fix] 튜토리얼 기능 구현 준비

--- star_project/Assets/3.Script/YG/Quest/MissionManager_.cs
+++ star_project/Assets/3.Script/YG/Quest/MissionManager_.cs
@@ -26,6 +26,7 @@ public class MissionManager_ : MonoBehaviour
 
     public void Setting()
     {
+        Debug.Log("설정 시작!");
         bool is_change = false;
 
         foreach (Mission_userdata data in BackendGameData_JGD.userData.quest_Info.userdata)
@@ -44,6 +45,8 @@ public class MissionManager_ : MonoBehaviour
             {
                 cur_missiontypes.Add(mission.criterion_type);
             }
+
+            Debug_mission(mission);
         }
 
         if (is_change)
@@ -52,21 +55,36 @@ public class MissionManager_ : MonoBehaviour
         }
     }
 
+    private void Debug_mission(Mission mission)
+    {
+        Debug.Log(mission.mission_id + "/" + mission.criterion_type);
+    }
+
+    public void Test_btn(int index)
+    {
+        Debug.Log(index + "/"+ (CriterionType)index);
+        Debug.Log(cur_missiontypes.Count);
+        Check_mission((CriterionType)index);
+    }
+
     //CriterionType에 맞는 수락한 미션이 있는지 확인
     //조건에서 해당 메서드 부르면 됨
     public void Check_mission(CriterionType type)
     {
         if (cur_missiontypes.Contains(type))
         {
+            Debug.Log("미션 있음");
             foreach (Mission_userdata data in BackendGameData_JGD.userData.quest_Info.userdata)
             {
                 if (data.criterion_type == type)
                 {
+                    Debug.Log("해당 미션 번호 : "+ data.mission_id);
                     data.criterion++;
                     data.Data_update();
                 }
             }
         }
+        Debug.Log("미션 없음");
     }
 
     public Mission_userdata Mission2data(Mission mission)
@@ -80,6 +98,23 @@ public class MissionManager_ : MonoBehaviour
         }
         Debug.Log("대응하는 Mission_userdata 없음");
         return null;
+    }
+
+    public void ALL_Accept_btn() //수락버튼 클릭 시 호출
+    {
+        for (int i = 0; i < BackendGameData_JGD.userData.quest_Info.userdata.Count; i++)
+        {
+            Accept_mission(i);
+        }
+        Debug.Log("모두 수락완료");
+    }
+
+    public void Accept_mission(int index)
+    {
+        Mission_userdata mission =BackendGameData_JGD.userData.quest_Info.userdata[index];
+        mission.is_accept = true;
+        cur_missiontypes.Add(mission.criterion_type);
+        Debug.Log("수락완료");
     }
 
 }
