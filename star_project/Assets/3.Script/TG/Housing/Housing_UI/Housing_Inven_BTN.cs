3343fd2453c3880e996f4316729f03152a3f07cc
jas03006 <jas03006@kaist.ac.kr>
2024-03-14 11:38:35 +0900

    [feat] scriptable object 변경

--- star_project/Assets/3.Script/TG/Housing/Housing_UI/Housing_Inven_BTN.cs
+++ star_project/Assets/3.Script/TG/Housing/Housing_UI/Housing_Inven_BTN.cs
@@ -13,7 +13,7 @@ public class Housing_Inven_BTN : MonoBehaviour, IPointerDownHandler
     [SerializeField] private TMP_Text usable_text;
     [SerializeField] private TMP_Text max_text;
     [SerializeField] private TMP_Text name_text;
-
+    [SerializeField] private ObjectsDatabaseSO db;
     public housing_itemID id { private set; get; }
     public int usable_cnt { private set; get; }
     public int max_cnt { private set; get; }
@@ -53,11 +53,8 @@ public class Housing_Inven_BTN : MonoBehaviour, IPointerDownHandler
         img.sprite = SpriteManager.instance.Num2Sprite(id);
         usable_cnt = use_cnt_;
         max_cnt = max_cnt_;
-        int ind_ = (int)id;
-        if (ind_ < 0 || ind_ >= name_arr.Length) {
-            ind_ = 0;
-        }
-        name_text.text = name_arr[ind_];
+        
+        name_text.text = db.get_object(id_).name;//name_arr[ind_];
         update_UI();
     }
 
